e64ed1e08ff95916a7cd8f510b7052b9c767bdbd
zahir <Zahirgeek@gmail.com>
2021-03-15 15:05:07 +0800

    Update random_copy-move.py

--- random_copy-move.py
+++ random_copy-move.py
@@ -11,8 +11,10 @@ parser.add_argument("input_files", type=str,
                     help="The path of the input files.")
 parser.add_argument("--output_files", type=str,
                     help="The path of the output files.")
-parser.add_argument("--num", type=int,
+parser.add_argument("--num", type=float, default=0,
                     help="The number of files you want to copy")
+parser.add_argument("--arithmetic", default=0,
+                    help="算式，支持乘法*和除法/")
 parser.add_argument("--type", type=str, default="c",
                     help="Copy or Move?, input c or copy for copy / m or move for move, default is copy")
 
@@ -20,8 +22,25 @@ args = parser.parse_args()
 
 input_path = os.path.join(args.input_files)
 output_path = os.path.join(args.output_files)
-num = args.num
+num = int(args.num)
+arithmetic = args.arithmetic
+
+if arithmetic != 0 and num == 0:
+    num_division = arithmetic.split('/')
+    num_multiplication = arithmetic.split('*')
+    if len(num_division) == 2:
+        num = float(num_division[0]) / float(num_division[1])
+    elif len(num_multiplication) == 2:
+        num = float(num_multiplication[0]) * float(num_multiplication[1])
+    else:
+        print('arithmetic参数输入错误，请重新输入')
+        sys.exit(1)
+    num = int(num)
+    
 type_ = args.type
+if num == 0:
+    print('未选中任何文件，请检查输入数量是否有误')
+    sys.exit(1)
 
 try:
     files_list = glob.glob(input_path+'/*')
